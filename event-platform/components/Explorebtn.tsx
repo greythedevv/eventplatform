5698e0b436c58c24644f3c0a2a66afe0a97596f1
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-05 13:01:01 +0100

    e

--- event-platform/components/Explorebtn.tsx
+++ event-platform/components/Explorebtn.tsx
@@ -5,7 +5,8 @@ import React from 'react'
 const Explorebtn = () => {
   return (
     <div>
-      <button type="button" id='explore-btn' className='mt-7 mx-auto' onClick={()=> console.log("click")}> <a href='#events'> Explore Events <img src="/icons/arrow-down.svg" alt="arrow-down" /></a></button>
+      <button type="button" id='explore-btn' className='mt-7 mx-auto' onClick={()=> console.log("click")}> <a href='#events'> Explore Events
+         <img src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/></a></button>
     </div>
   )
 }
